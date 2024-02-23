6fc4c4aebe78d6cb4f2f0e5ab7297879e11695f5
지원 <92582758+jasumine@users.noreply.github.com>
2024-02-23 17:43:18 +0900

    소켓을 이용한 tcp서버, 클라이언트
    1.서버와 클라이언트에서 소켓을 만든다.
    2. 서버에서 bind, listen을 해서 기다리고 있는다.
    3. 클라이언트는 서버의 ip주소와 포트번호로 접속이 가능하다. connect
    4. 연결된 경우 서버에서는 accept
    5. client가 연결을 해서 메세지를 전송한다. Send(sendBuffer)
    6. 서버는 receiveBuffer에 클라이언트가보낸 sendBuffer를 넣고 데이터를 출력한다. receiveData
    7. 서버에서도 sendBuffer로 클라이언트에 메세지를 보낸다. send(sendBuffer)
    8. 클라이언트에서 서버에서 보낸 메세지를 받아서 출력한다. recevieData(receiveBuffer)
    
    서버는 7번이후로 나머지 코드를 실행하고
    클라이언트는 8번이후로 나머지 코드를 실행한다.
    --> 서버와 클라이언트가 메세지를 주고 받기위해 뒤의 코드를 실행하지 않고 기다렸다면, 주고 받는게 끝난 경우에는 기다리지 않고 각자 코드를 실행해서 종료한다.

---
+++ TCPClient/TCPClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 클라이언트 소켓 생성
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // 서버 IP 주소 및 포트 번호 설정
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+
+            // 서버에 연결
+            clientSocket.Connect(ipEndPoint);
+
+            // 서버에게 데이터 전송
+            string sendData = "Hello, Server!";
+            byte[] sendBuffer = Encoding.UTF8.GetBytes(sendData);
+            clientSocket.Send(sendBuffer);
+
+            // 서버로부터 데이터 수신
+            byte[] receiveBuffer = new byte[1024];
+            int receiveBytes = clientSocket.Receive(receiveBuffer);
+
+            // 수신된 데이터 문자열로 변환
+            string receiveData = Encoding.UTF8.GetString(receiveBuffer, 0, receiveBytes);
+
+            Console.WriteLine("서버로부터 받은 데이터: " + receiveData);
+
+            // 소켓 닫기
+            clientSocket.Close();
+
+            Console.WriteLine("클라이언트가 종료되었습니다.");
+        }
+    }
+}
---
+++ TCPServer/TCPServer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 서버 소켓 생성
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // IP 주소 및 포트 번호 설정
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, 5000);
+
+            // 서버 소켓 바인딩
+            serverSocket.Bind(ipEndPoint);
+
+            // 클라이언트 연결 대기
+            serverSocket.Listen(10);
+
+            Console.WriteLine("서버가 시작되었습니다. 클라이언트 연결을 기다리고 있습니다.");
+
+            // 클라이언트 연결 수락
+            Socket clientSocket = serverSocket.Accept();
+
+            // 클라이언트로부터 데이터 수신
+            byte[] receiveBuffer = new byte[1024];
+            int receiveBytes = clientSocket.Receive(receiveBuffer);
+
+            // 수신된 데이터 문자열로 변환
+            string receiveData = Encoding.UTF8.GetString(receiveBuffer, 0, receiveBytes);
+
+            Console.WriteLine("클라이언트로부터 받은 데이터: " + receiveData);
+
+            // 클라이언트에게 데이터 전송
+            string sendData = "Hello, Client!";
+            byte[] sendBuffer = Encoding.UTF8.GetBytes(sendData);
+            clientSocket.Send(sendBuffer);
+
+            // 소켓 닫기
+            clientSocket.Close();
+            serverSocket.Close();
+
+            Console.WriteLine("서버가 종료되었습니다.");
+        }
+    }
+}
